7e87f98fe50296dbf1c58e9bed8b5d2afaad92f4
sitegui <sitegui@sitegui.com.br>
2023-12-09 00:05:44 +0100

    Add ElementPlus as web UI framework

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
+      <p>
+        <el-button>I'm a button</el-button>
+      </p>
+
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
