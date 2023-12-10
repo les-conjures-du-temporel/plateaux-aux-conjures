5c429312ead440d0269d1575e904ccfbba627e54
sitegui <sitegui@sitegui.com.br>
2023-12-10 12:56:15 +0100

    Revert "Failed attempt to use custom theme"
    This reverts commit 0068fd19cfda67913bc7c20546889ff50fc8e162.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import '~/styles/index.scss'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import router from './router'
 import App from '@/App.vue'
 import { Database } from '@/database'
-import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(ElementPlus)
 
 const db = new Database()
 app.provide('db', db)
--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
-import * as path from 'node:path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ElementPlus({
-      useSource: true,
-      defaultLocale: 'fr'
-    })
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`
-      }
-    }
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~/': `${path.resolve(__dirname, 'src')}/`
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
