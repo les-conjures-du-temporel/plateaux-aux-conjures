0068fd19cfda67913bc7c20546889ff50fc8e162
sitegui <sitegui@sitegui.com.br>
2023-12-10 12:55:56 +0100

    Failed attempt to use custom theme
    I couldn't find a way to make it work.
    
    Relevant documentation: https://element-plus.org/en-US/guide/theming.html

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import './assets/main.css'
+import '~/styles/index.scss'
 
 import { createApp } from 'vue'
 import router from './router'
 import App from '@/App.vue'
 import { Database } from '@/database'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 const db = new Database()
 app.provide('db', db)
