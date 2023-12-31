9c49753be832c12e66024facc3608119092cef49
Guilherme Souza <sitegui@sitegui.com.br>
2023-12-31 02:33:01 +0100

    Debug code to call function

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 import type { BggGame } from '@/board_game_geek'
-import { initializeApp } from 'firebase/app'
+import { type FirebaseApp } from 'firebase/app'
 import {
   collection,
   doc,
@@ -43,20 +43,7 @@ interface PlayActivity {
 export class Database {
   _firestore: Firestore
 
-  constructor() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyBg8ecSB_ogiqWAWCZgWRcOKtkrRFu6fOE',
-      authDomain: 'plateaux-aux-conjures.firebaseapp.com',
-      projectId: 'plateaux-aux-conjures',
-      storageBucket: 'plateaux-aux-conjures.appspot.com',
-      messagingSenderId: '656580485619',
-      appId: '1:656580485619:web:edaa857b984e6e1bc57286'
-    }
-
-    // Initialize Firebase
-    const firebaseApp = initializeApp(firebaseConfig)
-
-    // Initialize Cloud Firestore and get a reference to the service
+  constructor(firebaseApp: FirebaseApp) {
     this._firestore = getFirestore(firebaseApp)
   }
 
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/fr'
 import { Database, type Game, type Translations } from '@/database'
 import router from '@/router'
+import { initializeApp } from 'firebase/app'
+import { getFunctions, httpsCallable } from 'firebase/functions'
 
 const app = createApp(App)
 
@@ -19,7 +21,29 @@ app.use(Quasar, {
   lang: quasarLang
 })
 
-const db = new Database()
+const firebaseConfig = {
+  apiKey: 'AIzaSyBg8ecSB_ogiqWAWCZgWRcOKtkrRFu6fOE',
+  authDomain: 'plateaux-aux-conjures.firebaseapp.com',
+  projectId: 'plateaux-aux-conjures',
+  storageBucket: 'plateaux-aux-conjures.appspot.com',
+  messagingSenderId: '656580485619',
+  appId: '1:656580485619:web:edaa857b984e6e1bc57286'
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+const functions = getFunctions(firebaseApp)
+const recordPlayActivity = httpsCallable(functions, 'recordPlayActivity')
+
+// TODO: remove debug
+// TODO: set up app check: https://firebase.google.com/docs/app-check/cloud-functions
+// TODO: set up region: https://firebase.google.com/docs/functions/locations
+recordPlayActivity({ gameId: '262712', day: '2023-12-31', location: 'club' })
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => console.warn(error))
+
+const db = new Database(firebaseApp)
 const games: Ref<Game[]> = ref([])
 const translations: Ref<Translations> = ref({
   categories: new Map(),
