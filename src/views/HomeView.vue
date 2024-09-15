dc2dafa7d4b0d5c60f7e5d39123e043f785181ef
sitegui <sitegui@sitegui.dev>
2024-09-15 17:33:55 +0200

    Update dependencies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
 
 <template>
   <div class="text-center">
@@ -7,21 +11,21 @@
 
   <p class="text-h5">Nos outils pour les jeux de plateau</p>
 
-  <q-card class="q-my-sm cursor-pointer" @click="$router.push({ name: 'suggest' })">
+  <q-card class="q-my-sm cursor-pointer" @click="router.push({ name: 'suggest' })">
     <q-card-section>
       <div class="text-h6"><q-icon name="tips_and_updates" /> L'assistant</div>
       <div>Laisse-moi t'aider à choisir un jeu dans notre bibliothèque</div>
     </q-card-section>
   </q-card>
 
-  <q-card class="q-my-sm cursor-pointer" @click="$router.push({ name: 'games' })">
+  <q-card class="q-my-sm cursor-pointer" @click="router.push({ name: 'games' })">
     <q-card-section>
       <div class="text-h6"><q-icon name="flare" /> Les jeux du moment</div>
       <div>Vois à quoi on joue en ce moment</div>
     </q-card-section>
   </q-card>
 
-  <q-card class="q-my-sm cursor-pointer" @click="$router.push({ name: 'record-play' })">
+  <q-card class="q-my-sm cursor-pointer" @click="router.push({ name: 'record-play' })">
     <q-card-section>
       <div class="text-h6"><q-icon name="rate_review" /> Enregistre ta partie</div>
       <div>Dis-nous à quoi tu as joué avec nous</div>
